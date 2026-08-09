f00f1a232cecec7aad6a94421c67456f9dc9f9e0
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-08-09 13:33:15 -0700

    update logo path

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Overhead configurations for changing naming and other configurable fields
 export const APP_NAME: string = "ScamGuard";
-export const APP_EMBED_THUMBNAIL: string = "https://scamguard.app/assets/site-logo.png";
+export const APP_EMBED_THUMBNAIL: string = "https://scamguard.app/logo.png";
 export const TOS_LINK: string = "https://scamguard.app/terms";
 export const PRIVACY_LINK: string = "https://scamguard.app/privacy";
 
